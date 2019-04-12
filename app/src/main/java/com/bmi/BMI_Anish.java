9aecdc38617cb06e68df7dcfc1a0b930ff93ae07
Anishsthapit7199 <anishsthapit@outlook.com>
2019-04-12 15:29:21 +0545

    Initial commit

---
+++ app/src/main/java/com/bmi/BMI_Anish.java
@@ -0,0 +1,26 @@
+package com.bmi;
+
+class BMI_Anish {
+    private float height, weight;
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+    public float CalculateBMI(){
+        return weight/(height*height);
+    }
+}
+
+
